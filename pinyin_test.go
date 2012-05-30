41e93ada21b8982c21022a036e63c184006daa0a
haoqis <haoqis@gmail.com>
2012-05-31 03:05:12 +0800

    .

--- pinyin_test.go
+++ pinyin_test.go
@@ -8,11 +8,11 @@ var pinyinTests = map[string] string {
     "hello,世界!":"hello,ShiJie!", 
     "囧":"Jiong",
     "中国":"ZhongGuo",
-    "中&公":"Zhong&Gong",
     "中a国":"ZhongaGuo",
     "中a1国":"Zhonga1Guo",
     "a中a1国1":"aZhonga1Guo1",
 	"兲朝":"TianChao",
+	"和谐":"HeXie",
 	 
 }
 
