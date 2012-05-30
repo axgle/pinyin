96441679164fbaeae3481340cb3067c765f39633
haoqis <haoqis@gmail.com>
2012-05-31 00:42:11 +0800

    fmt

--- pinyin.go
+++ pinyin.go
@@ -1,48 +1,51 @@
 package pinyin
-import ( 
+
+import (
 	"github.com/axgle/mahonia"
-	"regexp" 
+	"regexp"
 )
+
 var hzRegexp = regexp.MustCompile("^[\u4e00-\u9fa5]$")
 
 var enc = mahonia.NewEncoder("gbk")
-func utf8ToGBK(s string) string{   
-   return enc.ConvertString(s)
-}	
+
+func utf8ToGBK(s string) string {
+	return enc.ConvertString(s)
+}
 
 // convert chinese to pinyin
 func Convert(s string) string {
-	pyString := ""	
-    var str string
+	pyString := ""
+	var str string
 	var chrAsc int
-	var i1,i2 int
-	
+	var i1, i2 int
+
 	for _, rune := range []rune(s) {
-	    str = string(rune)
-		if  hzRegexp.MatchString(str) {
-		gbkString  := utf8ToGBK(str)
- 
+		str = string(rune)
+		if hzRegexp.MatchString(str) {
+			gbkString := utf8ToGBK(str)
+
 			i1 = int(gbkString[0])
 			i2 = int(gbkString[1])
 			chrAsc = i1*256 + i2 - 65536
 			if chrAsc > 0 && chrAsc < 160 {
 				pyString += str
 			} else {
-				if   v,ok:=tableMap[chrAsc];ok {
-				   pyString += v
+				if v, ok := tableMap[chrAsc]; ok {
+					pyString += v
 				} else {
-				for i := (len(pyValue) - 1); i >= 0; i-- {
+					for i := (len(pyValue) - 1); i >= 0; i-- {
 
-					if pyValue[i] <= int(chrAsc) {
-						pyString += pyName[i]
-						break
+						if pyValue[i] <= int(chrAsc) {
+							pyString += pyName[i]
+							break
+						}
 					}
 				}
-				}
-			}		
-		} else{
-		  pyString += str
+			}
+		} else {
+			pyString += str
 		}
-	}	
+	}
 	return pyString
 }
